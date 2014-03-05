9ec1bc173aeb53af30c29014f5c63efa38729921
Jeff Hicks <jmhicks@gmail.com>
2014-03-04 19:15:06 -0500

    Initial commit

---
+++ src/main/java/org/nemac/geogaddi/Geogaddi.java
@@ -0,0 +1,23 @@
+package org.nemac.geogaddi;
+
+public class Geogaddi {
+
+	// command-line util
+	public static void main(String args[]) {
+		if (args.length != 1) {
+			System.out.println("Invalid arguments.");
+			System.out.println("Please format the command as follows: ");
+			System.out.println("java -jar geogaddi.jar <path/to/geogaddi.properties>");
+		}
+	}
+	
+	public static void processGeoData() {
+		
+	}
+	
+	private static void process() {
+		
+	}
+	
+	
+}
